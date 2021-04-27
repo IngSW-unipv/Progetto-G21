23b2e3ba893c376438931549b2497205e1e8a805
Daniele Cattabriga <daniele_cattabriga@libero.it>
2021-04-27 19:18:42 +0200

    Added embryo of controller with functioning menu parser, format to follow is defined by the txt file

--- src/menu/Tester.java
+++ src/menu/Tester.java
@@ -1,26 +1,58 @@
 package menu;
+import leRestaurant.RestaurantController;
+import java.io.*;
+import java.util.*;
 
 public class Tester {
 
 	public static void main(String[] args) {
-		String testing= "Spaghetti alle vongole,20.56,Ingredienti,Vongole,50,Sale,20,Zucchero,5,Spaghetti,100";
-		MenuEntry entryTest= new MenuEntry(testing);
-		System.out.println(entryTest.getDishName());
-		System.out.println(entryTest.getDishPrice());
-		System.out.println(entryTest.getDishIngredientsStringed());
-		entryTest.editEntry("spaget");
-		entryTest.editEntry(10.10);
-		try {
-			Double get= 20.00;
-			entryTest.editEntry("Vongole", get);
-		}
-		catch(IngredientOperationDeniedException e) {
-			System.out.println(e.getMessage());
+//		String testing= "Spaghetti alle vongole,15.2,Ingredienti,Spaghetti,100,Vongole,50,Sale,5,Olio,10";
+//		MenuEntry entryTest= new MenuEntry(testing);
+//		System.out.println(entryTest.getDishName());
+//		System.out.println(entryTest.getDishPrice());
+//		System.out.println(entryTest.getDishIngredientsStringed());
+//		entryTest.editEntry("spaget");
+//		entryTest.editEntry(10.10);
+//		try {
+//			Double get= 20.00;
+//			entryTest.editEntry("Vongole", get);
+//		}
+//		catch(IngredientOperationDeniedException e) {
+//			System.out.println(e.getMessage());
+//		}
+//		
+//		System.out.println(entryTest.getDishName());
+//		System.out.println(entryTest.getDishPrice());
+//		System.out.println(entryTest.getDishIngredientsStringed());
+//		
+//		String toSplit= "Spaghetti alle vongole		€15.20";
+//		String dishName=toSplit.substring(0, toSplit.indexOf("\t"));
+//		Double dishPrice=Double.parseDouble(toSplit.substring(toSplit.indexOf("€")+1));
+//		System.out.println(dishName);
+//		System.out.println(dishPrice);
+//		toSplit="Ingredienti: Spaghetti 100 g, Vongole 50 g, Sale 5 g, Olio 10 cl.";
+//		String[] buffer= toSplit.split(",");
+//		String dishEntry=dishName+","+dishPrice+",";
+//		for(String s: buffer) {
+//			System.out.println(s);
+//			s=s.replace(" ", ",");
+//			System.out.println(s);
+//			s=s.replace(":", "");
+//			System.out.println(s);
+//			dishEntry+=s.substring(0, s.lastIndexOf(","));
+//			System.out.println(s.substring(0, s.lastIndexOf(",")));
+//			System.out.println(dishEntry);
+//			
+//		}
+		
+		RestaurantController r= new RestaurantController();
+		r.generateMenu("/home/daniele/Progetto-G21/src/menu/MenuTest");
+		Collection<MenuEntry> c=r.getMenu();
+		for(MenuEntry me:c) {
+			System.out.println(me.getDishName()+"\t \t"+me.getDishPrice()+"\n"+me.getDishIngredientsStringed()+"\n");
 		}
 		
-		System.out.println(entryTest.getDishName());
-		System.out.println(entryTest.getDishPrice());
-		System.out.println(entryTest.getDishIngredientsStringed());
+			
 	}
 
 }
