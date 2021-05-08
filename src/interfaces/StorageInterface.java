b40199a69c2a734a9c40441cbbe4d5f10a22b39c
Daniele Cattabriga <daniele_cattabriga@libero.it>
2021-05-08 21:26:25 +0200

    Created Wrapper StoredIngredient, defined StorageInterface and test file for format

--- src/interfaces/StorageInterface.java
+++ src/interfaces/StorageInterface.java
@@ -1,5 +1,25 @@
 package interfaces;
+import java.util.LinkedHashMap;
 
 public interface StorageInterface /*Maybe it will be an observable, maybe not, gotta see */ {
+	
+	abstract boolean isDoable(LinkedHashMap<String, Double> ingredients);
+	abstract boolean requestSingleIngredient(String name, Double amount);
+	abstract void rewriteManifest(String path);
+	abstract void addIngredient(String name, Double amount);
+	abstract void editIngredientAmount(String name, Double amount);
+	abstract String[] checkIngredientsOnLowerBound();
+	abstract String[] checkIngredientsOnUpperBound();
+	abstract String[] checkIngredientsOnLowerBound(String[] ingredients);
+	abstract String[] checkIngredientsOnUpperBound(String[] ingredients);
+	abstract void removeIngredient(String name);
+	abstract LinkedHashMap<String, Double> getManifest();
+	abstract Double getSpecificIngredientAmount(String name);
+	abstract Double getSpecificIngredientLowerBound(String name);
+	abstract Double getSpecificIngredientUpperBound(String name);
+	abstract void printManifest();
+	abstract void loadBackup();
+	abstract void printManifest(String path);
+	abstract void loadBackup(String path);
 	
 }
