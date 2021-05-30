98668217a07514b6d034f2a9045a1aa6ddb8b12a
Ghelm99 <andrea.galmuzzi01@universitadipavia.it>
2021-05-30 15:38:48 +0200

    Something in Restaurant.java, first implementation of Table.java

--- src/model/Restaurant.java
+++ src/model/Restaurant.java
@@ -1,5 +1,21 @@
 package model;
 
 public class Restaurant {
+	
+	private static Restaurant istance = new Restaurant();
+	// String revenueSystemPath = "./RevenueSystem.ser";
+	// String inventoryPath = "./Inventory.ser";
+	// String restaurantMenuPath = "./Menu.ser";
+	
+	private Storage storage;
+	private Menu restaurantMenu;
+	// private ArrayList<Table> tables;
+	// private ArrayList<Table> emptyTables;
+	// private RevenueSystem revenueSystem;
+	// private StaffSystem staffSystem;
+	// private OrderSystem orderSystem; ???
+	
+	
+	
 
 }
