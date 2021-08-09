9fa05a9d59fb9f9283215aa7e6b1714c235a44b8
Ghelm99 <andrea.galmuzzi01@universitadipavia.it>
2021-08-09 17:09:42 +0200

    Some changes in Menu.java testers

--- src/testers/MenuTester.java
+++ src/testers/MenuTester.java
@@ -1,7 +1,6 @@
 package testers;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Collection;
 import java.util.Iterator;
 
@@ -13,27 +12,40 @@ public class MenuTester {
 	public static void main(String[] args) {
 
 		File menuFile = new File("Files/menuFile.txt");
-
-		if (menuFile.exists() == false) {
-			try {
-				menuFile.createNewFile();
-			} catch (IOException e) {
-				System.err.println(e.getMessage());
-			}
+		
+		if(menuFile.exists()) {
+			menuFile.delete();
 		}
-
+		
 		Menu menu = Menu.getInstance(menuFile.getAbsolutePath());
+
+		showMenu(menu);
+
+		menu.addMenuEntry("Pasta al pomodoro, 3");
+		menu.addMenuEntry("Pasta al pesto, 3");
+		menu.addMenuEntry("Tagliata di manzo, 8");
+		menu.addMenuEntry("Pizza margherita, 3.5");
+		menu.addMenuEntry("Tiramisù, 5");
+		menu.addMenuEntry("Pastiera napoletana, 4");
+
+		showMenu(menu);
 
 		menu.removeMenuEntry(0);
 
+		showMenu(menu);
+
+		menu.removeMenuEntry("Tagliata di manzo, 8");
+
+		showMenu(menu);
+	}
+
+	public static void showMenu(Menu menu) {
 		Collection<MenuEntry> menuCollection = menu.getEntriesCollection();
-
 		Iterator<MenuEntry> i = menuCollection.iterator();
-
 		while (i.hasNext()) {
 			System.out.println(i.next().toString());
 		}
-
+		System.out.println("\n");
 	}
 
 }
