d60c9bb9f9bcee5023311dbe64c56d6537f7a52a
Ghelm99 <andrea.galmuzzi01@universitadipavia.it>
2021-08-11 16:09:30 +0200

    Some fixes in SimpleBillTester.java

--- src/testers/SimpleBillTester.java
+++ src/testers/SimpleBillTester.java
@@ -1,41 +1,50 @@
 package testers;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 
 import model.Bill;
 import model.MenuEntry;
 import model.Order;
 import model.OrderManager;
 
+/* add -> see -> prepare -> deliver. La sequenzialità va rispettata! */
+
 public class SimpleBillTester {
 
 	public static void main(String[] args) {
 		Order o1 = new Order(1, new MenuEntry("Pasta al pomodoro, 5"));
 		Order o2 = new Order(1, new MenuEntry("Tiramisù, 8"));
-		Bill bill = new Bill(1);
+		// Bill bill = new Bill(1);
 
-		ArrayList<Order> orders = new ArrayList<Order>();
+		ArrayList<Order> orders = null;
 		OrderManager orderManager = OrderManager.getInstance();
-		orderManager.seeOrderToNotPrepared(o1);
-		orderManager.seeOrderToNotPrepared(o2);
 		orderManager.addOrder(o1);
 		orderManager.addOrder(o2);
+		orderManager.seeOrderToNotPrepared(o1);
+		orderManager.seeOrderToNotPrepared(o2);
 		orderManager.prepareOrder(o1);
 		orderManager.prepareOrder(o2);
 		orderManager.deliverOrder(o1);
 		orderManager.deliverOrder(o2);
 
-		orders.add(o1);
-		orders.add(o2);
+		// orders.add(o1);
+		// orders.add(o2);
 
-		System.out.println(orders);
-		System.out.println(bill.getOrders());
+		Bill bill = new Bill(1);
+		orders = bill.getOrders();
 
-		if (orders.equals(bill.getOrders())) {
-			System.out.println("OK");
-		} else {
-			System.out.println("TEST FAILED");
+		Iterator<Order> i = orders.iterator();
+		while (i.hasNext()) {
+			System.out.println(i.next().toString());
 		}
+
+		/*
+		 * System.out.println(orders); System.out.println(bill.getOrders());
+		 * 
+		 * if (orders.equals(bill.getOrders())) { System.out.println("OK"); } else {
+		 * System.out.println("TEST FAILED"); }
+		 */
 	}
 
 }
