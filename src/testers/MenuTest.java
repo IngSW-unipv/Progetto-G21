b903e0262e630e5ad026a18220cff9c7cd9d0309
unknown <gaia.vezzolo01@universitadipavia.it>
2021-08-11 23:03:59 +0200

    Added uncompleted class MenuTest

---
+++ src/testers/MenuTest.java
@@ -0,0 +1,44 @@
+package testers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+import org.junit.Test;
+
+import model.Menu;
+import model.MenuEntry;
+import model.EntryDoesNotExistException;
+
+public class MenuTest {
+
+	Menu menu = Menu.getInstance("Files/menuFile.txt");
+	
+	MenuEntry entry1 = new MenuEntry("Pasta al pesto", 3);
+	
+	@Test
+	public void testGetSpecificMenuEntry()
+	{
+		assertEquals(entry1.toString(), menu.getSpecificMenuEntry(0).toString());
+	}
+	
+	
+	@Test
+	public void testGetSpecificMenuEntryName()
+	{
+		assertEquals(entry1.getDishName(), menu.getSpecificMenuEntryName(0));
+	}
+	
+	
+	@Test
+	public void testGetSpecificMenuEntryPrice()
+	{
+		assertEquals(entry1.getDishPrice(), menu.getSpecificMenuEntryPrice(0));
+	}
+	
+	
+//	@Test(expected = EntryDoesNotExistException.class)
+//	public void testGetSpecificMenuEntryPrice1()
+//	{
+//		menu.getSpecificMenuEntryPrice(8);
+//	}
+}
