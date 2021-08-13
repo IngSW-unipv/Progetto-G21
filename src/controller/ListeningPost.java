1ca09fbe1e13c5ebc4399182e72d2876a49862df
Daniele Cattabriga <daniele_cattabriga@libero.it>
2021-08-13 14:38:48 +0200

    Merged ThreadedServerSocket with ListeningPost for streamlining reasons.

--- src/controller/ListeningPost.java
+++ src/controller/ListeningPost.java
@@ -1,4 +1,13 @@
 package controller;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.regex.Pattern;
 
 /**
  * The ListeningPost class. Once instantiated, it can dynamically change the
@@ -6,16 +15,31 @@ package controller;
  * program.
  */
 
-public class ListeningPost {
+public class ListeningPost extends Thread {
 	private static ListeningPost post;
 	private SystemController controller;
+	private ServerSocket server;
+	private Socket kitchenClient;
+	private BufferedReader bufferRead;
+	private BufferedWriter bufferWrite;
+	
 
 	/**
 	 * ListeningPost default constructor (private in order to respect Singleton
 	 * pattern).
 	 */
 	private ListeningPost() {
-		ThreadedServerSocket.callServer();
+		try {
+			kitchenClient = server.accept();
+			BufferedReader bufferRead = new BufferedReader(new InputStreamReader(kitchenClient.getInputStream()));
+			BufferedWriter bufferWrite = new BufferedWriter(new OutputStreamWriter(kitchenClient.getOutputStream()));
+			System.out.println("Server running!\n");
+			System.out.println(server.getLocalSocketAddress());
+			System.out.println(server.getLocalPort());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
 	}
 
 	/**
@@ -27,6 +51,7 @@ public class ListeningPost {
 	public static ListeningPost getInstance() {
 		if (post == null) {
 			post = new ListeningPost();
+			post.start();
 		}
 		return post;
 	}
@@ -59,11 +84,38 @@ public class ListeningPost {
 	}
 
 	/**
-	 * Simple method to notify the server of an outbound message.
-	 * 
-	 * @param message specifies the message to send to the server.
+	 * The thread in this server is used to keep a constant, unhindered, eye on the
+	 * input buffer, so as to immediately perform the required services when the
+	 * message arrives, through invocation of the ListeningPost.
 	 */
-	public void notifyServer(String message) {
-		ThreadedServerSocket.callServer().sendMessage(message);
+	public void run() {
+		while (true) {
+			Pattern p = Pattern.compile("^([a-zA-Z0-9]+, )+[a-zA-Z0-9]+$");
+			String[] unpackedMessage;
+			try {
+				String message;
+				while ((message = bufferRead.readLine()) == null)
+					; // is ok???? 
+					  /*yes, the readline returns a null value if the file it's
+					  // empty, putting this construct here blocks the thread while the buffer is empty, as soon
+					  // as a new message comes through the input channel, the thread restarts. -Daniele */
+				if (p.matcher(message).matches()) {
+					unpackedMessage = message.split(", ");
+					notifyListeningPost(unpackedMessage[0],Arrays.copyOfRange(unpackedMessage, 1, unpackedMessage.length - 1));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}
+	
+	/** Small method that writes a message to the socket. */
+	public synchronized void sendMessage(String message) {
+		try {
+			bufferWrite.write(message + "\n");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
