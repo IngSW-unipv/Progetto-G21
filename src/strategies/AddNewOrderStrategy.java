89799a1868dd9cd8a491f51d36f569332778c1ad
Ghelm99 <andrea.galmuzzi01@universitadipavia.it>
2021-08-14 09:46:27 +0200

    Adding first full implementation of AddNewOrderStrategy

--- src/strategies/AddNewOrderStrategy.java
+++ src/strategies/AddNewOrderStrategy.java
@@ -1,6 +1,9 @@
 package strategies;
 
 import controller.Restaurant;
+import model.MenuEntry;
+import model.Order;
+import model.OrderManager;
 
 public class AddNewOrderStrategy extends StrategyAbstract {
 	private static AddNewOrderStrategy instance = null;
@@ -16,22 +19,23 @@ public class AddNewOrderStrategy extends StrategyAbstract {
 		return instance;
 	}
 
+	/** In this scenario, args[0]: tableNum, args[1]: dishEntry. */
 	@Override
 	public void execute(String[] args) {
-		// TODO Auto-generated method stub
-
+		OrderManager orderManagerInstance = Restaurant.getInstance().getOrderManager();
+		orderManagerInstance.addOrder(new Order(Integer.parseInt(args[0]), new MenuEntry(args[1])));
 	}
 
-	/**
-	 * In this scenario, args[0]: tableNum, args[1]:
+	/*
+	 * In this scenario,args[0]:tableNum,args[1]:
 	 * 
 	 * @Override public void execute(String[] args) { SystemController
-	 *           controllerInstance = super.getController(); Menu menuInstance =
-	 *           super.getController().getRestaurant().getRestaurantMenu();
-	 *           controllerInstance.getRestaurant().getOrderManager() .addOrder(new
-	 *           Order(Integer.parseInt(args[0]), )));
+	 * controllerInstance = super.getController(); Menu menuInstance =
+	 * super.getController().getRestaurant().getRestaurantMenu();
+	 * controllerInstance.getRestaurant().getOrderManager() .addOrder(new
+	 * Order(Integer.parseInt(args[0]), )));
 	 * 
-	 *           }
+	 * }
 	 */
 
 }
