336dd58d687980fbbada677b2a2b7043c43225cb
Ghelm99 <andrea.galmuzzi01@universitadipavia.it>
2021-08-15 21:27:40 +0200

    Adding some main javaFx classes for WaitersControlPanel and ChefsControlPanel

---
+++ src/chefsProgram/view/ChefsControlPanelMain.java
@@ -0,0 +1,27 @@
+package chefsProgram.view;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ChefsControlPanelMain extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("ChefsControlPanel.fxml"));
+			Scene scene = new Scene(root);
+			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
