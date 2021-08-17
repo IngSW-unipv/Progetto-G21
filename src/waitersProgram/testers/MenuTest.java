386b7ee09918e7c3182a545df09550d58399990a
Ayaril <didyk.iry@gmail.com>
2021-08-17 11:46:47 +0200

    Refactor of Menu testers, silly comments update

--- src/waitersProgram/testers/MenuTest.java
+++ src/waitersProgram/testers/MenuTest.java
@@ -13,59 +13,89 @@ public class MenuTest {
 
 	File menuFile = new File("Files/menuFile.txt");
 	Menu menu = Menu.getInstance();
+	MenuEntry entry1 = new MenuEntry("Pasta al pesto", 3.0);
 
-//	Menu menu = Menu.getInstance("Files/menuFile.txt");
-
-	MenuEntry entry1 = new MenuEntry("Pasta al pesto", 3);
-
+	void resetMenu() {
+		if (menuFile.exists()) {
+			menuFile.delete();
+		}
+		menu.addMenuEntry(entry1.toString());
+		menu.addMenuEntry("Pasta al pomodoro, 3");
+		menu.addMenuEntry("Pizza margherita, 5");
+		menu.addMenuEntry("Tiramisù, 5");
+		menu.addMenuEntry("Pastiera napoletana, 4");
+	}
+	
+	/**
+	 * @Test of method getSpecificMenuEntry().
+	 */
 	@Test
-	public void testGetSpecificMenuEntry() {
+	public void testGetSpecificMenuEntryEntryKey() {
 		assertEquals(entry1.toString(), menu.getSpecificMenuEntry(0).toString());
 	}
 
+	/**
+	 * @Test of method getSpecificMenuEntryName().
+	 */
 	@Test
 	public void testGetSpecificMenuEntryName() {
 		assertEquals(entry1.getDishName(), menu.getSpecificMenuEntryName(0));
 	}
 
+	/**
+	 * @Test of method getSpecificMenuPrice().
+	 */
 	@Test
 	public void testGetSpecificMenuEntryPrice() {
 		assertEquals(entry1.getDishPrice(), menu.getSpecificMenuEntryPrice(0));
 	}
 
-//	@Test(expected = EntryDoesNotExistException.class)
-//	public void testGetSpecificMenuEntryPrice1()
-//	{
-//		menu.getSpecificMenuEntryPrice(8);
-//	}
+	/**
+	 * @Test of method getMenuFilePath().
+	 */
+	@Test
+	public void testGetMenuFilePath() {
+		assertEquals("Files/menuFile.txt", menu.getMenuFilePath());
+	}
 
+	/**
+	 * @Test of method addMenuEntry().
+	 */
 	@Test
 	public void testAddMenuEntry() {
+		resetMenu();
 		String dishEntry = "Pollo con patatine, 10";
 		menu.addMenuEntry(dishEntry);
 		assertEquals(dishEntry.toString(), (menu.getSpecificMenuEntry(menu.getFileLinesCounter() - 1)).toString());
 	}
-//	
-//
-//	@Test(expected = EntryDoesNotExistException.class)
-//	public void testRemoveMenuEntry()
-//	{
-//		menu.removeMenuEntry(5);
-//		menu.getSpecificMenuEntry(5);
-//	}
 
+	/**
+	 * @Test of method removeMenuEntry().
+	 */
+	@Test
+	public void testRemoveMenuEntry() {
+		menu.removeMenuEntry("Pastiera napoletana, 4");
+		assertEquals(null, menu.getSpecificMenuEntry("Pastiera napoletana, 4"));
+	}
+
+	/**
+	 * @Test of method EditSpecificMenuEntryName().
+	 */
 	@Test
 	public void testEditSpecificMenuEntryName() {
 		String newName = "Tagliata di manzo con limone";
-		menu.editSpecificMenuEntry(2, newName);
+		menu.editSpecificMenuEntry(1, newName);
 		assertEquals(newName.toString(), menu.getSpecificMenuEntryName(menu.getFileLinesCounter() - 1));
-
 	}
 
+	/**
+	 * @Test of method addMenuEntry().
+	 */
 	@Test
 	public void testEditSpecificMenuEntryPrice() {
-		double newPrice = 7;
-		menu.editSpecificMenuEntry(2, newPrice);
+		double newPrice = 7.0;
+		menu.editSpecificMenuEntry(1, newPrice);
 		assertEquals(newPrice, menu.getSpecificMenuEntryPrice(menu.getFileLinesCounter() - 1));
 	}
+
 }
--- src/waitersProgram/testers/SimpleMenuTester.java
+++ src/waitersProgram/testers/SimpleMenuTester.java
@@ -10,16 +10,12 @@ import waitersProgram.model.MenuEntry;
 public class SimpleMenuTester {
 
 	public static void main(String[] args) {
-
 		File menuFile = new File("Files/menuFile.txt");
+		Menu menu = Menu.getInstance();
 
 		if (menuFile.exists()) {
 			menuFile.delete();
 		}
-
-		Menu menu = Menu.getInstance();
-
-		showMenu(menu);
 
 		menu.addMenuEntry("Pasta al pomodoro, 3");
 		menu.addMenuEntry("Pasta al pesto, 3");
@@ -28,15 +24,67 @@ public class SimpleMenuTester {
 		menu.addMenuEntry("Tiramisù, 5");
 		menu.addMenuEntry("Pastiera napoletana, 4");
 
-		showMenu(menu);
+		/**
+		 * Tester of method getSpecificMenuEntry().
+		 */
+		if (menu.getSpecificMenuEntry(0).toString().equals("Pasta al pomodoro, 3")) {
+			System.out.println("OK");
+		} else {
+			System.out.println("TEST FAILED");
+		}
 
+		/**
+		 * Tester of method getSpecificMenuEntryName().
+		 */
+		if (menu.getSpecificMenuEntryName(0).toString().equals("Pasta al pomodoro")) {
+			System.out.println("OK");
+		} else {
+			System.out.println("TEST FAILED");
+		}
+
+		/**
+		 * Tester of method getSpecificMenuEntryPrice().
+		 */
+		if (menu.getSpecificMenuEntryPrice(0) == 3) {
+			System.out.println("OK");
+		} else {
+			System.out.println("TEST FAILED");
+		}
+
+		/**
+		 * Tester of method getMenuFilePath().
+		 */
+		if (menu.getMenuFilePath().equals("Files/menuFile.txt")) {
+			System.out.println("OK");
+		} else {
+			System.out.println("TEST FAILED");
+		}
+
+		/**
+		 * Tester of method addMenuEntry().
+		 */
+		String dishEntry = "Pollo con patatine, 10";
+		menu.addMenuEntry(dishEntry);
+
+		if (menu.getSpecificMenuEntry(menu.getFileLinesCounter() - 1).toString().equals(dishEntry.toString())) {
+			System.out.println("OK");
+		} else {
+			System.out.println("TEST FAILED");
+		}
+
+		/**
+		 * Tester of method removeMenuEntry(). -- OK
+		 */
+
+		menu.removeMenuEntry("Pastiera napoletana, 4");
+		menu.removeMenuEntry("Pasta al pesto, 3");
 		menu.removeMenuEntry(0);
 
-		showMenu(menu);
-
-		menu.removeMenuEntry("Tagliata di manzo, 8");
-
-		showMenu(menu);
+		/**
+		 * Catching the exception
+		 */
+		menu.removeMenuEntry("Pastiera napoletana, 4");
+		menu.removeMenuEntry("Pastiera napoletana, 4");
 	}
 
 	public static void showMenu(Menu menu) {
