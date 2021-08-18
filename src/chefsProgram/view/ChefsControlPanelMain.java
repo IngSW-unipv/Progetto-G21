8f26c3fc868f8ec4cb33ed28b1bcc17fe26ddd65
unknown <gaia.vezzolo01@universitadipavia.it>
2021-08-18 12:33:07 +0200

    added a CSS file for ChefsControlPanel, modified ChefsControlPanel.fxml and ChefsControlPanelMain.java

--- src/chefsProgram/view/ChefsControlPanelMain.java
+++ src/chefsProgram/view/ChefsControlPanelMain.java
@@ -1,26 +1,26 @@
 package chefsProgram.view;
-
+	
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
+
 
 public class ChefsControlPanelMain extends Application {
-
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage primaryStageChefsControlPanel) {
 		try {
-			Parent root = FXMLLoader.load(getClass().getResource("ChefsControlPanel.fxml"));
-			Scene scene = new Scene(root);
-			// scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch (Exception e) {
+	        Parent rootChefsControlPanel = FXMLLoader.load(getClass().getResource("/chefsProgram/view/ChefsControlPanel.fxml"));
+			Scene sceneChefsControlPanel = new Scene(rootChefsControlPanel,800,600);
+			sceneChefsControlPanel.getStylesheets().add(getClass().getResource("chefsControlPanel.css").toExternalForm());
+			primaryStageChefsControlPanel.setScene(sceneChefsControlPanel);
+			primaryStageChefsControlPanel.show();
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
-
+	
 	public static void main(String[] args) {
 		launch(args);
 	}
