82b3825779f6a1116178408f0583877d2c926337
Ghelm99 <andrea.galmuzzi01@universitadipavia.it>
2021-08-31 18:47:18 +0200

    Erasing javadoc

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"chefsProgram.controller"},{"l":"chefsProgram.model"},{"l":"chefsProgram.view"},{"l":"waitersProgram.controller"},{"l":"waitersProgram.model"},{"l":"waitersProgram.strategies"},{"l":"waitersProgram.testers.model"},{"l":"waitersProgram.view"}];updateSearchResults();
